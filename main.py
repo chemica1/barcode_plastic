5ee7851dd57aa4798a88e3b302092010a5ca31b0
meoho <46700473+chemica1@users.noreply.github.com>
2020-04-03 19:01:52 +0900

    .
    .

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-9791160501483
+import winsound
+
+
+def reversedString(s):
+    return s[::-1]
+
+def main():
+    n = '0'
+    while len(n.strip()) > 0: ##아무 입력없이(or공백만입력하고) 엔터누르면 반복문 종료
+        n = input()
+        print(reversedString(n))
+        if n == '7610700009338':
+            winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
+
+if __name__ == '__main__':
+    main()
